37ef48fde73dfd1c5d1c885b3758e2e073a4142f
raf-bergs <r0752792@student.thomasmore.be>
2023-01-18 14:26:56 +0100

    homepage created with two carousels

---
+++ p4-frontend/components/layout/productGroup.js
@@ -0,0 +1,45 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Image from "next/image";
+export default function ProductGroup({ title, products }) {
+  const { responsive } = products;
+  return (
+    <div
+      className={
+        "flex flex-col justify-center items-center overflow-hidden p-2"
+      }>
+      <div className="bg-slate-200 w-full rounded-t-lg">
+        <h3 className="text-center text-black">{title}</h3>
+      </div>
+      <Carousel
+        showArrows={true}
+        showIndicators={false}
+        infiniteLoop={true}
+        dynamicHeight={false}
+        showThumbs={false}
+        autoPlay={true}
+        interval={5000}
+        className={"w-full"}>
+        {responsive.map((item) => (
+          <div
+            key={item.id}
+            className={
+              "text-center text-lg text-white flex flex-col justify-center items-center bg-gradient-to-r from-slate-400 to-slate-500 rounded-b-lg"
+            }>
+            <div className={"w-4/5 p-1 mt-2"}>
+              <img
+                src={item.imageUrl}
+                alt="slides"
+                className="object-cover aspect-square object-center"
+              />
+            </div>
+            <div>
+              <h2>{item.title}</h2>
+              <p>{item.text}</p>
+            </div>
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+}
--- p4-frontend/pages/index.js
+++ p4-frontend/pages/index.js
@@ -2,9 +2,17 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
+import ProductGroup from "@/components/layout/productGroup";
+
+import { items, items2 } from "../public/Items.json";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="grid grid-cols-2">
+      <ProductGroup title="For you" products={items} />
+      <ProductGroup title="Deals" products={items2} />
+    </div>
+  );
 }
