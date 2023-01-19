abd873daa505f66774c89bc9742e9bf7fbeb4dc0
raf-bergs <r0752792@student.thomasmore.be>
2023-01-19 09:00:12 +0100

    started on contactpage

--- p4-frontend/pages/about/index.js
+++ p4-frontend/pages/about/index.js
@@ -1,3 +1,11 @@
 export default function AboutPage() {
-  return <h1>About Page</h1>;
+  return (
+    <div className="   flex  justify-center w-screen">
+      <div className="w-1/2 bg-slate-500 text-center rounded-md h-full m-10">
+        <h1>About us</h1>
+        <p>We are a company that sells so many things, pretty epic.</p>
+        <p>Contact Info: 📞: +32478-00-00-00 📧:info@company.com</p>
+      </div>
+    </div>
+  );
 }
