215136c5f0968a0ca4300cfb648c59dc82f045f4
BogdanLazar01 <R0779031@student.thomasmore.be>
2023-01-20 13:54:54 +0100

    Now listing categories from Contentful

--- p4-frontend/pages/Categories/index.js
+++ p4-frontend/pages/Categories/index.js
@@ -1,4 +1,3 @@
-import { getAllCategories } from "@/dummy-data";
 import Link from "next/link";
 
 export default function CategoriesPage(props) {
@@ -9,20 +8,25 @@ export default function CategoriesPage(props) {
       <h1 className="text-3xl font-bold">Categories</h1>
       <ul>
         {categories.map((category) => (
-          <li key={category.id}><Link href={`/Categories/${category.id}`}>{category.name}</Link></li>
+          <li key={category.sys.id}>
+            <Link href={`/Categories/${category.sys.id}`}>{category.fields.title}</Link>
+          </li>
         ))}
       </ul>
     </div>
   );
 }
 
-export async function getStaticProps() {
-  const categories = getAllCategories();
+const client = require("contentful").createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+});
 
+export async function getStaticProps() {
+  const categories = await client.getEntries({content_type: 'category'});
   return {
     props: {
-      categories: categories,
-    },
-    revalidate: 60,
+      categories: categories.items,
+    }
   };
 }
