fabc06cd1542c4a7cdf7bc684c8943dea5efaff4
TimGrevendonk <r0831309@student.thomasmore.be>
2023-01-22 14:34:45 +0100

    swagger: the best i can get it

--- p4_backend/src/main/java/fact/it/p4_backend/config/SwaggerConfig.java
+++ p4_backend/src/main/java/fact/it/p4_backend/config/SwaggerConfig.java
@@ -1,9 +1,8 @@
 package fact.it.p4_backend.config;
 
-import org.springframework.context.annotation.Configuration;
 
-import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,24 +11,25 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+//@Configuration
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
-    public Docket api(){
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .apis(RequestHandlerSelectors.basePackage("fact.it.backend"))
+                .paths(PathSelectors.regex("/api*"))
                 .build();
     }
 
-    private ApiInfo apiInfo(){
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Products API")
-                .description("This page list all the rest apis for the products API.")
-                .version("1.0-SNAPSHOT")
+                .title("Elision API")
+                .description("List all endpoints of the Elision project")
+                .version("1.0")
                 .build();
     }
 }
-
+// http://localhost:8080/swagger-ui.html
