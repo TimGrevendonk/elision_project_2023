1e0e2a8ab43a3a5ac5175265124d439667d83777
BogdanLazar01 <R0779031@student.thomasmore.be>
2023-01-25 14:14:28 +0100

    Now you can search for products!

---
+++ p4-frontend/data/build-search.js
@@ -0,0 +1,73 @@
+// build-search.js
+const dotenv = require("dotenv");
+const algoliasearch = require("algoliasearch/lite")
+
+const client = require("contentful").createClient({
+  space: "n80dqssuk9ot",
+  environment: "master",
+  accessToken: "oxbuUp5Nh3Rd9EV2mhBj_QFF_IkcdWsRuYagNQi0iHs",
+});
+
+async function getProducts() {
+  const productsClient = [];
+  await client
+    .getEntries({ content_type: "product" })
+    .then((response) => {
+      return response.items.map((product) => {
+        console.log(`productId: ${product}`),
+          productsClient.push(product);
+      });
+    })
+    .catch(console.error);
+  return productsClient;
+}
+
+function transformProductsToSearchObjects(products) {
+  const transformed = products.map((product) => {
+    return {
+      objectID: product.sys.id,
+      title: product.fields.title,
+      description: product.fields.description,
+      price: product.fields.price,
+      rating: product.fields.rating,
+      ratings: product.fields.rates,
+    };
+  });
+
+  return transformed;
+}
+
+(async function () {
+  dotenv.config();
+
+  try {
+    const products = await getProducts();
+    const transformed = transformProductsToSearchObjects(products);
+
+    // initialize the client with your environment variables
+    const client = algoliasearch(
+      "EL070LM1BO",
+      "28d547e1454fe21df43b2adc325c9775"
+    );
+
+    // initialize the index with your index name
+    const index = client.initIndex("d1_tm_team_b3");
+
+    // save the objects!
+    const algoliaResponse = await index.saveObjects(transformed);
+
+    // check the output of the response in the console
+    console.log(
+      `🎉 Sucessfully added ${
+        algoliaResponse.objectIDs.length
+      } records to Algolia search. Object IDs:\n${algoliaResponse.objectIDs.join(
+        "\n"
+      )}`
+    );
+
+    // we have data ready for Algolia!
+    console.log(transformed);
+  } catch (error) {
+    console.log(error);
+  }
+})();
