a67b2ebcc9728561bd3598adb6c3e6ef32bf1724
Raf Bergs <r0752792@student.thomasmore.be>
2023-01-29 14:54:32 +0100

    can add user to DB via frontEnd

--- p4-frontend/pages/sign-up/index.js
+++ p4-frontend/pages/sign-up/index.js
@@ -22,7 +22,22 @@ export default function SignUpPage() {
                   id="email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="name@company.com"
-                  required=""
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="username"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  placeholder="username"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
                 />
               </div>
               <div>
@@ -37,22 +52,7 @@ export default function SignUpPage() {
                   id="password"
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="confirm-password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Confirm password
-                </label>
-                <input
-                  type="confirm-password"
-                  name="confirm-password"
-                  id="confirm-password"
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
+                  required
                 />
               </div>
               <div className="flex items-start">
@@ -62,7 +62,7 @@ export default function SignUpPage() {
                     aria-describedby="terms"
                     type="checkbox"
                     className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                    required=""
+                    required
                   />
                 </div>
                 <div className="ml-3 text-sm">
@@ -72,7 +72,7 @@ export default function SignUpPage() {
                     I accept the{" "}
                     <Link
                       className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                      href="#">
+                      href="/terms-conditions">
                       Terms and Conditions
                     </Link>
                   </label>
@@ -99,7 +99,28 @@ export default function SignUpPage() {
   );
 }
 
-function submitHandler(event) {
-  console.log("no backend yet");
+async function submitHandler(event) {
   event.preventDefault();
+
+  const newUserData = {
+    name: event.target.username.value,
+    mail: event.target.email.value,
+    password: event.target.password.value,
+  };
+
+  const JSONdata = JSON.stringify(newUserData);
+
+  const endpoint = process.env.NEXT_PUBLIC_JAVA_BASE_LINK + "/user/create";
+
+  console.log(endpoint);
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSONdata,
+  };
+
+  const response = await fetch(endpoint, options);
 }
