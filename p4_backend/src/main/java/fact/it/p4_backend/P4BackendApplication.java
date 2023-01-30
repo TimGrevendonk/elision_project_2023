e6dd350a6b4ab9e42750220ae5a25005bd90dbc6
raf-bergs <r0752792@student.thomasmore.be>
2023-01-30 15:18:39 +0100

    working on comments

--- p4_backend/src/main/java/fact/it/p4_backend/P4BackendApplication.java
+++ p4_backend/src/main/java/fact/it/p4_backend/P4BackendApplication.java
@@ -1,7 +1,11 @@
 package fact.it.p4_backend;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class P4BackendApplication {
--- p4_backend/src/main/java/fact/it/p4_backend/config/SecurityConfig.java
+++ p4_backend/src/main/java/fact/it/p4_backend/config/SecurityConfig.java
@@ -1,13 +1,16 @@
 package fact.it.p4_backend.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
