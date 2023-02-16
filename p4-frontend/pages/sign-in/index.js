e19e7107e618e99b66f15ae6ab0781d64b0bf64f
TimGrevendonk <r0831309@student.thomasmore.be>
2023-02-16 21:20:28 +0100

    fixed some style theme and removed pay tab

--- p4-frontend/pages/sign-in/index.js
+++ p4-frontend/pages/sign-in/index.js
@@ -24,18 +24,18 @@ export default function SingInPage() {
     }
   }
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className=" bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full  rounded-lg shadow  border md:mt-0 sm:max-w-md xl:p-0  bg-gray-800  border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center">
+            <h1 className="text-xl font-bold leading-tight tracking-tight  md:text-2xl  text-white text-center">
               Sign in to your account
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={submitHandler}>
               <div>
                 <label
                   htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium   text-white"
                 >
                   Your email
                 </label>
@@ -43,7 +43,7 @@ export default function SingInPage() {
                   type="email"
                   name="email"
                   id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className=" border   sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  bg-gray-700  border-gray-600  placeholder-gray-400  text-white  focus:ring-blue-500  focus:border-blue-500"
                   placeholder="name@company.com"
                   required=""
                 />
@@ -51,7 +51,7 @@ export default function SingInPage() {
               <div>
                 <label
                   htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium   text-white"
                 >
                   Password
                 </label>
@@ -60,7 +60,7 @@ export default function SingInPage() {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="  border    sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  bg-gray-700  border-gray-600  placeholder-gray-400  text-white  focus:ring-blue-500  focus:border-blue-500"
                   required=""
                 />
               </div>
@@ -68,7 +68,7 @@ export default function SingInPage() {
                 <div className="flex items-start"></div>
                 <Link
                   href="/sign-in/forgot-password"
-                  className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  className="text-sm font-medium text-primary-600 hover:underline  text-primary-500"
                 >
                   Forgot password?
                 </Link>
@@ -79,11 +79,11 @@ export default function SingInPage() {
               >
                 Sign in
               </button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+              <p className="text-sm font-light    text-gray-400">
                 Need an account?{" "}
                 <Link
                   href="/sign-up"
-                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  className="font-medium text-primary-600 hover:underline  text-primary-500"
                 >
                   Sign up here
                 </Link>
