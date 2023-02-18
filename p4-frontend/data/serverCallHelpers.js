097f3c0e2d3fa7899cd765ab483af69108577b1f
Brent Druyts <r0831536@student.thomasmore.be>
2023-02-18 09:00:00 +0000

    serverCallHelpers.js edited online with Bitbucket

--- p4-frontend/data/serverCallHelpers.js
+++ p4-frontend/data/serverCallHelpers.js
@@ -1,8 +1,12 @@
 const urlPrefix = process.env.NEXT_PUBLIC_JAVA_BASE_LINK;
 
 export async function callServerPost(url, data) {
+
+  // code brent
+  console.log("functie 1");
   console.log("[debug] url for backend", urlPrefix + url);
   console.log("[debug] url for backend", data);
+  
   const response = await fetch(urlPrefix + url, {
     method: "POST",
     body: data ? JSON.stringify(data) : "",
@@ -15,7 +19,11 @@ export async function callServerPost(url, data) {
 }
 
 export async function callServerGet(url) {
+
+  // code brent
+  console.log("functie 2");
   console.log("[debug] url for backend", urlPrefix + url);
+  
   const response = await fetch(urlPrefix + url, {
     method: "GET",
     headers: {
@@ -26,8 +34,12 @@ export async function callServerGet(url) {
 }
 
 export async function callServerPostNoJson(url, data) {
+
+  // code brent
+  console.log("functie 3");
   console.log("[debug] url for backend", urlPrefix + url);
   console.log("[debug] url for backend", data);
+  
   const response = await fetch(urlPrefix + url, {
     method: "POST",
     body: data ? JSON.stringify(data) : "",
