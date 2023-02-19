c1077271821378bacf93625af3d2dcc55b832b37
TimGrevendonk <r0831309@student.thomasmore.be>
2023-02-19 17:50:03 +0100

    fixed sing up styling

--- p4-frontend/pages/sign-up/success.js
+++ p4-frontend/pages/sign-up/success.js
@@ -1,11 +1,25 @@
 import Link from "next/link";
 
 export default function SuccessSignUpPage() {
+  function handleReturnToPageBeforeLogin(event) {
+    window.history.go(-2);
+  }
   return (
-    <div>
-      <p>your account was created successfully!</p>
-      <p>Please log yourself in to confirm user creation.</p>
-      <Link href="/">Back to homepage</Link>
-    </div>
+    <>
+      <div className="container m-auto">
+        <h1 className="pb-1 text-center">
+          your account was created successfully!
+        </h1>
+        <p className="pb-1 text-center">
+          Please log yourself in to confirm user creation.
+        </p>
+        <p
+          className="cursor-pointer bg-slate-800 rounded-sm text-center p-1 mt-2 hover:bg-slate-600 md:w-1/2 md:m-auto"
+          onClick={handleReturnToPageBeforeLogin}
+        >
+          return to previous page
+        </p>
+      </div>
+    </>
   );
 }
